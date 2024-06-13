efe363e315647d5e656c7456a4f5282b4fde9fa6
qqalex <86507185+qqalex@users.noreply.github.com>
2024-06-13 14:00:21 -0400

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,30 @@
+# Generate valid background check (cyberbackgroundchecks.com) queries from name, address, phone, or email data
+# OSINT Lookup / Automation Tool using CyberBackgroundChecks.com
+
+# Alex (github.com/qqalex)
+# June 13th 2024
+
+def searchByName(fName=None, mName=None, lName=None, city=None, state=None):
+    query = "https://www.cyberbackgroundchecks.com/people/"
+
+    if lName == None:
+        return
+    
+    query += fName + mName + lName
+
+    if state is not None:
+        query += "/" + state
+    elif city is not None:
+        query += "/" + city
+    
+    return query
+
+
+def searchByAddress(address, city, state):
+    return f"https://www.cyberbackgroundchecks.com/address/{address}-{city}-{state}"
+
+def searchByPhone(phoneNum):
+    return f"https://www.cyberbackgroundchecks.com/phone/{phoneNum}"
+
+def searchByEmail(email):
+    return f"https://www.cyberbackgroundchecks.com/email/{email}"
